cccdb7577d6fe6a217241d5c7474d0d2d8b2920b
eliseoluna <eliseo.luna2002@gmail.com>
2026-04-15 14:47:21 -0500

    Add files via upload

---
+++ morse-code-generator/frmMain.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblPlainTxt = new System.Windows.Forms.Label();
+            this.lblMorse = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.txtText = new System.Windows.Forms.TextBox();
+            this.lblOutput = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblPlainTxt
+            // 
+            this.lblPlainTxt.AutoSize = true;
+            this.lblPlainTxt.Location = new System.Drawing.Point(24, 35);
+            this.lblPlainTxt.Name = "lblPlainTxt";
+            this.lblPlainTxt.Size = new System.Drawing.Size(57, 13);
+            this.lblPlainTxt.TabIndex = 0;
+            this.lblPlainTxt.Text = "Plain Text:";
+            // 
+            // lblMorse
+            // 
+            this.lblMorse.AutoSize = true;
+            this.lblMorse.Location = new System.Drawing.Point(24, 91);
+            this.lblMorse.Name = "lblMorse";
+            this.lblMorse.Size = new System.Drawing.Size(67, 13);
+            this.lblMorse.TabIndex = 1;
+            this.lblMorse.Text = "Morse Code:";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(339, 389);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 2;
+            this.btnConvert.Text = "C&onvert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(467, 389);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "C&lear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(632, 389);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 4;
+            this.btnExit.Text = "E&xit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // txtText
+            // 
+            this.txtText.Location = new System.Drawing.Point(87, 32);
+            this.txtText.Name = "txtText";
+            this.txtText.Size = new System.Drawing.Size(620, 20);
+            this.txtText.TabIndex = 5;
+            this.txtText.TextChanged += new System.EventHandler(this.txtText_TextChanged);
+            this.txtText.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtText_KeyPress);
+            // 
+            // lblOutput
+            // 
+            this.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOutput.Location = new System.Drawing.Point(24, 121);
+            this.lblOutput.Name = "lblOutput";
+            this.lblOutput.Size = new System.Drawing.Size(683, 246);
+            this.lblOutput.TabIndex = 6;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(743, 450);
+            this.Controls.Add(this.lblOutput);
+            this.Controls.Add(this.txtText);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblMorse);
+            this.Controls.Add(this.lblPlainTxt);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "International Morse Code Generator";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPlainTxt;
+        private System.Windows.Forms.Label lblMorse;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.TextBox txtText;
+        private System.Windows.Forms.Label lblOutput;
+    }
+}
+
---
+++ morse-code-generator/frmMain.cs
@@ -0,0 +1,159 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+//using System.Drawing;
+//using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMain : Form
+    {
+        #region declarations section
+
+        public bool blnConverted = false; // Indicates if a Morse conversion has occured
+        private static Regex _isChar = new Regex("^[,.?0-9a-zA-Z ]$"); // User input validation
+
+        public string[,] arrMorse = { {",", "--..--" }, {".", ".-.-.-" }, {"?", "..--.." }, {"0", "-----" },
+                                    {"1", ".----" }, {"2", "..---"}, {"3", "...--" }, {"4", "....-" },
+                                    {"5", "....." }, {"6", "-...."}, {"7", "--..." }, {"8", "---.." },{"9", "----." },
+                                    {"A", ".-" }, {"B", "-..." }, {"C", "-.-." }, {"D", "-.." },
+                                    {"E", "." }, {"F", "..-."}, {"G", "--."}, {"H", "...."},
+                                    {"I", ".." }, {"J", ".---"}, {"K", "-.-"}, {"L", ".-.."},
+                                    {"M", "--" }, {"N", "-."}, {"O", "---"}, {"P", ".--."},
+                                    {"Q", "--.-" }, {"R", ".-."}, {"S", "..."}, {"T", "-"},
+                                    {"U", "..-" }, {"V", "...-"}, {"W", ".--"}, {"X", "-..-"},
+                                    {"Y", "-.--" }, {"Z", "--.."}, {" ", "0"} };
+        #endregion
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+        #region frmMain_Load()
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+        #endregion
+
+        #region btnExit_Click()
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        #endregion
+
+        #region btnClear_Click()
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtText.Text = string.Empty;
+            lblOutput.Text = string.Empty;
+            txtText.Focus();
+        }
+        #endregion
+
+        #region parseString()
+        private void parseString()
+        {
+            string strPlainText = txtText.Text; // Get the user text - spaces = new line
+            string strCode = string.Empty;
+            blnConverted = true;
+
+            try
+            {
+                for (int z = 0; z <= strPlainText.Length - 1; z++) // Count the nubmer of elems needed iun picMorse array
+                {
+                    strCode = getCode(strPlainText[z]); // Get the numeric equivalent to morse code for each letter
+                    if (strCode == "0")
+                    {
+                        lblOutput.Text += "\n";
+                    }
+                    else
+                    {
+                        lblOutput.Text += strCode.PadRight(6);
+                    }
+                }
+            }
+            catch (Exception ex) // Handle the exception
+            {
+                MessageBox.Show("Generic Exception Handler: ", ex.ToString());
+            }
+        }
+        #endregion
+
+        #region getCode()
+        private string getCode(char strLetter) // Looks up character and find the morse numveric code from array
+        {
+            string strReturn = string.Empty;
+
+            for (int y = 0; y <= arrMorse.Length - 1; y++)
+            {
+                if (arrMorse[y,0] == Convert.ToString(char.ToUpper(strLetter)))
+                {
+                    strReturn = arrMorse[y,1];
+                    break;
+                }
+            }
+            return strReturn;
+        }
+        #endregion
+
+        #region btnConvert_Click
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            parseString();
+
+            if (blnConverted == true)
+            {
+                btnConvert.Enabled = false; // Set button States
+                btnClear.Enabled = true;
+            }
+        }
+        #endregion
+
+        #region IsInputAlphabetical()
+        public static bool IsInputAlphabetical(string str)
+        {
+            Match m = _isChar.Match(str); // Validate
+            return m.Success;
+        }
+        #endregion
+
+        #region txtText_TextChanged()
+        private void txtText_TextChanged(object sender, EventArgs e)
+        {
+            if (txtText.Text.Trim().Length > 0) // Set button button states
+            {
+                btnClear.Enabled = true;
+                btnConvert.Enabled = true;
+            }
+            else // No chars
+            {
+                btnClear.Enabled = false;
+                btnConvert.Enabled = false;
+                btnClear.PerformClick(); // Calls clear button click event
+            }
+        }
+        #endregion
+
+        #region txtText_KeyPress()
+        private void txtText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (IsInputAlphabetical(e.KeyChar.ToString()) == false)
+            {
+                if (Convert.ToInt32(e.KeyChar) != 8) //Allows the backspace - ASCII 8 
+                {
+                    e.Handled = true;
+                }
+                else if (Convert.ToInt32(e.KeyChar) == 8 && blnConverted == true)
+                    btnClear.PerformClick(); // Calls clear button click event
+            }
+        }
+        #endregion
+
+    }
+}
